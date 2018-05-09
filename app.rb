a40af98e9c5176a2fb8277addad5fe02a748d092
Michelle Olivieri <michelleo72@gmail.com>
2018-05-09 15:28:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Michelle."
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I Still Haven't Found What I'm Looking For."
+  end
+
 end
